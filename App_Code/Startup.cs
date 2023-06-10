8db910392e38341249ecd781bddda54653cb13e0
hmh25 <hmh25@DESKTOP-RYZEN>
2023-06-10 05:03:23 +0300

    Add project files.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Customer_Web_Project.Startup))]
+namespace Customer_Web_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
